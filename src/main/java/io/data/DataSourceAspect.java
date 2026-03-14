a7ba3037de0d90bb45a5ea5b4d5eccd9c4792b1f
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2026-03-14 18:31:09 +0530

    fix: update dependencies and configurations

--- src/main/java/io/data/DataSourceAspect.java
+++ src/main/java/io/data/DataSourceAspect.java
@@ -1,51 +1,43 @@
 package io.data;
 
-import io.data.DataSourceConfig.AspectRoutingContext;
+import java.util.Optional;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
 
-// @Aspect
-// @Component
+@Aspect
+@Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class DataSourceAspect {
-  @Around("readPointcut()")
-  public Object setRead(ProceedingJoinPoint pjp) throws Throwable {
-    try {
-      AspectRoutingContext.requestRO();
-      return pjp.proceed();
-    } finally {
-      AspectRoutingContext.clear();
-    }
+
+  private final RetryTemplate retryTemplate;
+  private static final Logger log = org.slf4j.LoggerFactory.getLogger(DataSourceAspect.class);
+
+  public DataSourceAspect(RetryTemplate retryTemplate) {
+    this.retryTemplate = retryTemplate;
   }
 
-  @Around("writePointcut()")
-  public Object setWrite(ProceedingJoinPoint pjp) throws Throwable {
-    try {
-      AspectRoutingContext.requestRW();
-      return pjp.proceed();
-    } finally {
-      AspectRoutingContext.clear();
-    }
-  }
+  @Pointcut("within(*..*Service) || within(*..*svc)")
+  public void servicePointcut() {}
 
-  @Pointcut(
-      "execution(* org.springframework.data.repository.Repository+.find*(..)) || execution(* org.springframework.data.repository.Repository+.get*(..))")
-  void readPointcut() {}
-
-  @Pointcut(
-      "execution(* org.springframework.data.repository.Repository+.save*(..)) || execution(* org.springframework.data.repository.Repository+.delete*(..))")
-  void writePointcut() {}
-
-  @Around("@annotation(ReadOnly) || @within(ReadOnly)")
-  public Object routeToRead(ProceedingJoinPoint pjp) throws Throwable {
-    try {
-      AspectRoutingContext.requestRO();
-      return pjp.proceed();
-    } finally {
-      AspectRoutingContext.clear();
-    }
+  //  @Around("servicePointcut()")
+  @Around(
+      "@annotation(org.springframework.stereotype.Service) || @within(org.springframework.stereotype.Service)")
+  public Object wrapper(ProceedingJoinPoint pjp) throws Throwable {
+    return retryTemplate.execute(
+        _ -> pjp.proceed(),
+        recovery -> {
+          log.error(
+              "All retry attempts are either exhausted: {} or the circuit is open",
+              recovery.getRetryCount(),
+              recovery.getLastThrowable());
+          return Optional.empty();
+        });
   }
 }
